0fee13a34ff592ce0e1a45f8607994dd66eb71ea
Lilia de Tassia <lylyatavares@gmail.com>
2024-05-17 15:02:23 -0300

    font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
 import { Inter } from 'next/font/google';
- 
+
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = 'someValue'; // Certifique-se de que a variável `lusitana` está corretamente exportada
